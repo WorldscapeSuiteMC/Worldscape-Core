b17b6f8e23e4f6ea2786eacc577145cc371c9454
SiliconianAuthor <worldscapesuitemc@gmail.com>
2026-07-03 15:45:24 -0700

    lang space tweaks

--- src/main/java/io/github/worldscapesuitemc_byte/worldscape_core/java/JCore.java
+++
@@ -1,8 +0,0 @@
-package io.github.worldscapesuitemc_byte.worldscape_core.java;
-public class JCore {
-    static void main() {
-        Entity entity = new Player(100.0D);
-        entity.doSomething(-2.0D);
-    }
-
-}
